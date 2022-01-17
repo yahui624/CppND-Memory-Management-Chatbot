91440ead8ee7d789a14102edfc6248b5bf23fd6a
root <yahui624@vt.edu>
2022-01-17 16:09:55 +0000

    Task1: Exclusive Ownership 1

--- src/chatgui.h
+++ src/chatgui.h
@@ -29,7 +29,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
